d61386c9f6cdbf3353d968323fbbe06acaf0372b
Victoria Cepeda <85692351+victoriacepeda@users.noreply.github.com>
2023-04-18 15:17:54 -0300

    agrega funcionalidad

--- tarjetasdesplegables.js
+++ tarjetasdesplegables.js
@@ -3,15 +3,15 @@
 const $verMasBtn = document.querySelectorAll('.mas-btn');
 const $texto = document.querySelectorAll('.oculto');
 
-//$verMasBtn.addEventListener('click', toggle);
+
 
 document.querySelectorAll('.mas-btn').forEach(item => {
     item.addEventListener('click', event => {
-      toggle()
+      console.log(event.target)
+      console.dir(event.target);
     })
   })
 
 function toggle(){
     $texto.classList.toggle('mostrar');
 }
-
